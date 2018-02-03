dbae0a367ed09bf8bc2f878bac974d98e6e3fe7c
Ted Lorts <tplorts@gmail.com>
2018-02-02 18:06:30 -0800

    add a T icon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
+import faviconICO from '../static/icons/favicon.ico'
 import './global.scss'
 
 
@@ -15,7 +16,9 @@ const TemplateWrapper = ({ children }) => (
           content: 'Software Product Engineer/Developer; Web & Mobile, Full Stack',
         },
       ]}
-    />
+    >
+      <link rel="shortcut icon" type="image/x-icon" href={ faviconICO } />
+    </Helmet>
     <div>
       {children()}
     </div>
